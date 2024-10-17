7e474d42c0cf72cc9e9c77422d190f9100d4b9c0
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-10-17 22:00:02 +0200

    file committed

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
  const submit = document.getElementById('mySubmit');
  const resultElement=  document.getElementById('resultElement'); 
  
- let age = 0;
+ 
 submit.addEventListener('click',function(){
   checkAge()
 })
 
-
+checkAge = function(){
+    if(age>=18){
+        console.log('you are allowed to vote')
+    
+    }else{
+        console.log('you are not allowed to vote');
+    }
+}
